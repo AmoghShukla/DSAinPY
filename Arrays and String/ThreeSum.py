073bc43055083f091dfeb590cc1ef9dfe5997c30
Amogh Shukla <amoghshukla548@gmail.com>
2026-02-24 11:43:58 +0530

    Better Solution for 3 Sum Added

--- Arrays and String/ThreeSum.py
+++ Arrays and String/ThreeSum.py
@@ -12,14 +12,35 @@ The distinct triplets are [-1,0,1] and [-1,-1,2].
 Notice that the order of the output and the order of the triplets does not matter.
 
 '''
+# Bruteforce
 
 def threeSum(nums: list, target: int):
-    final_Output = []
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            for k in range(j+1, len(nums)):
-                    if nums[i] + nums[j]+ nums[k] == target:
-                        final_Output.append([nums[i] , nums[j] , nums[k]])
+    final_Output: list = []
+    length: int = len(nums)
+    i: int = 0 
+    j: int = i+1
+    k: int = length - 1
+    while (i <= length - 2 and j <= length-1 and k <= length):
+        
+        if nums[i] + nums[j] + nums[k] == target:
+            final_Output.append([nums[i] , nums[j] , nums[k]])
+            if k == length - 1 and j == k-1 and i == j - 1:
+                break
+            if k == j:
+                i += 1
+                j = i + 1
+                k = length - 1
+            else:
+                k -= 1
+        elif nums[i] + nums[j] + nums[k] != target:
+            if k == length - 1 and j == k-1 and i == j - 1:
+                break
+            if k == j:
+                i += 1
+                j = i + 1
+                k = length - 1
+            else:
+                k -= 1
     return final_Output
 
 nums = [1,2,3,4,5,6,7,8,9]

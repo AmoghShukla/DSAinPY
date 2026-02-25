ad8fed1ce56f1f62ee1ff0e3cf90c8ad7c2c5197
Amogh Shukla <amoghshukla548@gmail.com>
2026-02-25 22:15:07 +0530

    Maximum Subarray with the Subarray Added

--- Arrays and String/6) MaximumSubarray.py
+++ Arrays and String/6) MaximumSubarray.py
@@ -10,13 +10,28 @@ Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 '''
 
 
-def MaxSubarray(nums: list[int]) -> int:
+def MaxSubarray(nums: list[int]):
     current_sum = nums[0]
     total_sum = nums[0]
-    for num in nums:
-        current_sum = max(num, current_sum + num)
-        total_sum = max(total_sum, current_sum)
-    return total_sum
+
+    temp_start = 0
+    start_index = 0
+    end_index = 0
+
+    for i in range(1, len(nums)):
+
+        if current_sum + nums[i] < nums[i]:
+            current_sum = nums[i]
+            temp_start = i
+        else:
+            current_sum += nums[i]
+
+        if current_sum > total_sum:
+            total_sum = current_sum
+            start_index = temp_start
+            end_index = i
+
+    return nums[start_index:end_index + 1], total_sum
 
 
 nums = [-2,1,-3,4,-1,2,1,-5,4]

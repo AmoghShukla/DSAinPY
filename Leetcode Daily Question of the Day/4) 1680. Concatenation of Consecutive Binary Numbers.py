d8c4a0585078e75d2aaaa2f7ea3d5b91412bebe9
Amogh Shukla <amoghshukla548@gmail.com>
2026-02-28 19:13:52 +0530

    Leetcode Question of the day : 28-02-2026 : Question Added

--- Leetcode Daily Question of the Day/4) 1680. Concatenation of Consecutive Binary Numbers.py
+++ Leetcode Daily Question of the Day/4) 1680. Concatenation of Consecutive Binary Numbers.py
@@ -1,3 +1,14 @@
+'''
+Given an integer n, return the decimal value of the binary string formed by concatenating 
+the binary representations of 1 to n in order, modulo 109 + 7.
+
+Input: n = 12
+Output: 505379714
+Explanation: The concatenation results in "1101110010111011110001001101010111100".
+The decimal value of that is 118505380540.
+After modulo 109 + 7, the result is 505379714.
+'''
+
 def concatenatedBinary(n: int) -> int:
     string = ""
     for i in range(1, n+1):

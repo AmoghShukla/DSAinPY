6520d704ac0bd89c5455f9045f61b860d90a4d29
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-02 23:25:21 +0530

    Leetcode Question of the Day Added : 02-02-2026

--- Leetcode Daily Question of the Day/6) 1536. Minimum Swaps to Arrange a Binary Grid.py
+++ Leetcode Daily Question of the Day/6) 1536. Minimum Swaps to Arrange a Binary Grid.py
@@ -11,9 +11,35 @@ Input: grid = [[0,0,1],[1,1,0],[1,0,0]]
 Output: 3
 '''
 
-def minSwaps(self, grid: list[list[int]]) -> int:
-    counter = 0
-    length = len(grid)
+class Solution:
+    def minSwaps(self, grid):
+        n = len(grid)
+        zeros = []
+
+        for row in grid:
+            count = 0
+            for j in range(n - 1, -1, -1):
+                if row[j] == 0:
+                    count += 1
+                else:
+                    break
+            zeros.append(count)
+
+        swaps = 0
+
+        for i in range(n):
+            needed = n - i - 1
+            j = i
+            while j < n and zeros[j] < needed:
+                j += 1
+            if j == n:
+                return -1
+            while j > i:
+                zeros[j], zeros[j - 1] = zeros[j - 1], zeros[j]
+                j -= 1
+                swaps += 1
+
+        return swaps
 
 
 grid = [[0,0,1],[1,1,0],[1,0,0]]

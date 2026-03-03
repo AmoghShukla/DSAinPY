fa0feb28a68f996db3669c46118df761810255d1
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-03 07:47:29 +0530

    Leetcode Question of the Day : 03-03-2026 : Question Added

---
+++ Leetcode Daily Question of the Day/7) 1545. Find Kth Bit in Nth Binary String.py
@@ -0,0 +1,17 @@
+'''
+Question No. : 1545
+Title : Find Kth Bit in Nth Binary String
+Difficulty : Medium
+
+Given two positive integers n and k, the binary string Sn is formed as follows:
+S1 = "0"
+Si = Si-1 + "1" + reverse(invert(Si-1)) for i > 1
+Where + denotes the concatenation operation, reverse(x) returns the reversed string x, and invert(x) inverts all the bits in x (0 changes to 1 and 1 changes to 0).
+For example, S2 = "0" + "1" + reverse(invert("0")) = "0" + "1" + reverse("1") = "0" + "1" + "1" = "011".
+Return the kth bit in Sn. It is guaranteed that k is valid for the given n.
+
+Example 1:
+Input: n = 3, k = 1
+Output: "0"
+Explanation: S3 is "0111001". The 1st bit is "0".
+'''

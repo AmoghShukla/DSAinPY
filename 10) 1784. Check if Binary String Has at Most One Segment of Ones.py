cbe3eeef2886da572a16c4551445b9792b337db1
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-06 22:51:57 +0530

    Leetcode Question of the Day : 06-03-2026 : Question Added

--- 10) 1784. Check if Binary String Has at Most One Segment of Ones.py
+++ 10) 1784. Check if Binary String Has at Most One Segment of Ones.py
@@ -1,3 +1,17 @@
+'''
+Question No. : 1784
+Title : Check if Binary String Has at Most One Segment of Ones
+Difficulty : Easy
+
+Given a binary string s, return true if all the 1's in s are grouped into a single segment.
+ In other words, return true if s contains at most one contiguous segment of 1's.
+Example 1:
+Input: s = "1001"
+Output: false
+Explanation: The 1's are not in a single segment.
+
+'''
+
 def checkOnesSegment(s: str) -> bool:
     seen_zero = False
 

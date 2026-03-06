c10a0f5f5e7e06590fbf8f0d883929ab5feb4ca1
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-06 23:10:34 +0530

    Linked List Cycle : Question Added

--- Linked List/2) 141. Linked List Cycle.py
+++ Linked List/2) 141. Linked List Cycle.py
@@ -1,3 +1,20 @@
+'''
+Question No. : 141
+Title : Linked List Cycle
+Difficulty : Easy
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to.
+Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+'''
+
 def hasCycle(head : ListNode) -> bool:
     if not head:
         return False

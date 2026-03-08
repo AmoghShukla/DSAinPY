4775b6192f87a4b31ce03cb551dbf10237b3ce30
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-08 13:39:29 +0530

    leetcode Question of The Day : 08-03-2026 : Question  Added

--- Leetcode Daily Question of the Day/11) 1980. Find Unique Binary String.py
+++ Leetcode Daily Question of the Day/11) 1980. Find Unique Binary String.py
@@ -1,3 +1,17 @@
+'''
+Question Number : 1980
+Title : Find Unique Binary String
+Difficulty : Medium
+
+Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. 
+If there are multiple answers, you may return any of them.
+Example 1:
+Input: nums = ["01","10"]
+Output: "11"
+Explanation: "11" does not appear in nums. "00" would also be correct.
+
+'''
+
 def findDifferentBinaryString(nums: list[str]) -> str:
     length = len(nums[0])
     val = 2 ** length

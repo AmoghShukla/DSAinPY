5c11e3376bcd2c77b93e4c8857e35ae6f19daae1
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-08 19:10:38 +0530

    Finding All Disappeared Numbers : Question Added

--- Arrays and String/14) 448. Find All Numbers Disappeared in an Array.py
+++ Arrays and String/14) 448. Find All Numbers Disappeared in an Array.py
@@ -1,3 +1,17 @@
+'''
+Question Number : 448
+Title : Find All Numbers Disappeared in an Array
+Difficulty : Easy
+
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Explanation: The numbers 5 and 6 do not appear in the array.
+
+'''
+
+
 def findDissappearedNums(nums: list) -> list:
     Missing : list = []
     length : int  = len(nums)

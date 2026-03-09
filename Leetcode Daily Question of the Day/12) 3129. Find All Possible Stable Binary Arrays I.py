6743466ebd1840e605ef74dc3cb19254b4a50324
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-09 07:47:06 +0530

    Leetcode Question of the Day : 09-03-2026 : Question  Added

--- Leetcode Daily Question of the Day/12) 3129. Find All Possible Stable Binary Arrays I.py
+++ Leetcode Daily Question of the Day/12) 3129. Find All Possible Stable Binary Arrays I.py
@@ -1,3 +1,19 @@
+'''
+Question Number : 3129
+Title : Find All Possible Stable Binary Arrays I
+Difficulty : Hard
+
+You are given three integers zero, one, and limit. You have zero 0's and one 1's. You want to construct a binary array that contains exactly zero 0's and one 1's, and the number of consecutive 0's or 1's in the array should be at most limit.
+Return the number of such binary arrays. Since the answer may be large, return it modulo 10^9 + 7.
+Example 1:
+Input: zero = 2, one = 1, limit = 2
+Output: 3
+Explanation: The possible binary arrays are [0,0,1], [0,1,0], and [1,0,0]. Note that [0,0,1] is not a valid array since it contains three consecutive 0's.
+
+
+'''
+
+
 from functools import lru_cache
 
 def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:

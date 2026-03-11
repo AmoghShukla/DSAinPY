ac48f03f1ce45c1a747b98e60f000b995d1cfd07
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-12 00:07:25 +0530

    Valid Parenthesis : Question Added

---
+++ Stack and Queue/1) 20. Valid Parenthesis.py
@@ -0,0 +1,13 @@
+'''
+Question : 20
+Title : Valid Parenthesis
+Difficulty : Easy
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Example 1:
+Input: s = "()"
+Output: true
+'''

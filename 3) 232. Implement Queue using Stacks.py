c918e45f89abe6e149536a6c7e7284e44fdca2fa
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-12 00:00:01 +0530

    Implement Queue using Stacks : Question Added

---
+++ 3) 232. Implement Queue using Stacks.py
@@ -0,0 +1,14 @@
+'''
+Question No. : 232
+Title : Implement Queue using Stacks
+Difficulty : Easy
+
+Implement a first in first out (FIFO) queue using only two stacks. 
+The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Example 1:
+Input : ["MyQueue","push","push","peek","pop","empty"]
+[[],[1],[2],[],[],[]]
+
+Output : [null,null,null,1,1,false]
+'''

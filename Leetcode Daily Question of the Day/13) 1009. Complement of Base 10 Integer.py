76115f30afa7a144de9ca5af8fa7e4cb2bfac697
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-11 08:42:37 +0530

    Leetcode Question of the day : 11-03-2026 : Question Added

---
+++ Leetcode Daily Question of the Day/13) 1009. Complement of Base 10 Integer.py
@@ -0,0 +1,13 @@
+'''
+Question No. : 1009.
+Title :  Complement of Base 10 Integer
+Difficulty : Easy
+
+Given a base-10 integer n, return the base-10 integer denoting its complement. 
+The complement of a base-10 integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+Example 1:
+Input: n = 5
+Output: 2
+
+'''

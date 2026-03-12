f42953144d2f4acb54c07431417520f37832452e
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-12 23:17:23 +0530

    Implement Stack Using Queues : Question Added

--- Stack and Queue/4) 225. Implement Stack using Queues.py
+++ Stack and Queue/4) 225. Implement Stack using Queues.py
@@ -1,3 +1,17 @@
+'''
+Question No. : 225
+Title : Implement Stack using Queues
+Difficulty : Easy
+
+Implement a last in first out (LIFO) stack using only two queues. 
+The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Example 1:
+Input : ["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output : [null, null, null, 2, 2, false]
+'''
+
 class MyStack:
 
     def __init__(self):

b817f0b988dfcb3139b570cf89dac9d9f1b73ed8
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-14 22:11:40 +0530

    Leetcode Question of the Day : Testcase  Added

--- Leetcode Daily Question of the Day/15) 1415. The k-th Lexicographical String of All Happy Strings of Length n.py
+++ Leetcode Daily Question of the Day/15) 1415. The k-th Lexicographical String of All Happy Strings of Length n.py
@@ -1,3 +1,21 @@
+'''
+Question No. : 1415
+Title : The k-th Lexicographical String of All Happy Strings of Length n
+Difficulty : Medium
+A happy string is a string that:
+consists only of letters of the set ['a', 'b', 'c'].
+s[i] != s[i + 1] for all values of i from 1 to s.length - 2 (inclusive).
+For example, "abc", "ac", "b" and "abcbab" are happy strings and "aa", "baa" and "ababbc" are not happy strings.
+Given two integers n and k, consider a list of all happy strings of length n sorted in lexicographical order.
+Return the kth string of this list or return an empty string if there are less than k happy strings of length n.
+Example 1:
+Input: n = 1, k = 3
+Output: "c"
+
+
+'''
+
+
 def getHappyString(n: int, k : int) -> str:
     res = []
 

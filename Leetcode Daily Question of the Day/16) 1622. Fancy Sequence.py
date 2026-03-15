0dc4c3e0ab04ac10c0e9f17270c57680a6e1f890
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-15 17:41:34 +0530

    Leetcode Question of the Day : Question Added

---
+++ Leetcode Daily Question of the Day/16) 1622. Fancy Sequence.py
@@ -0,0 +1,24 @@
+'''
+Question No. : 1622
+Title : Fancy Sequence
+Difficulty : Hard
+
+Write an API that generates fancy sequences using the append, addAll, and multAll operations.
+
+Implement the Fancy class:
+
+Fancy() Initializes the object with an empty sequence.
+void append(val) Appends an integer val to the end of the sequence.
+void addAll(inc) Increments all existing values in the sequence by an integer inc.
+void multAll(m) Multiplies all existing values in the sequence by an integer m.
+int getIndex(idx) Gets the current value at index idx (0-indexed) of the sequence modulo 109 + 7. If the index is greater or equal than the length of the sequence, return -1.
+
+Example 1:
+Input
+["Fancy", "append", "addAll", "append", "multAll", "getIndex", "addAll", "append", "multAll", "getIndex", "getIndex", "getIndex"]
+[[], [2], [3], [7], [2], [0], [3], [10], [2], [0], [1], [2]]
+
+Output
+[null, null, null, null, null, 10, null, null, null, 50, 80, 140]
+
+'''

28c82b389e4029bbb9ad911c22ca77b1f599a9ac
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-16 22:36:25 +0530

    Valid Pallindrome : Question Added

--- Arrays and String/15) 125. Valid Palindrome.py
+++ Arrays and String/15) 125. Valid Palindrome.py
@@ -1,3 +1,18 @@
+'''
+Question No. : 125
+Title : Valid Palindrome
+Difficulty : Easy
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+'''
+
+
 def ValidPalindrome(s: str) -> bool:
     left, right = 0, len(s) - 1
 

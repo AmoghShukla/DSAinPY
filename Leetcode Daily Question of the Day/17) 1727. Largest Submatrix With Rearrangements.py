23e9377419152d4ef00993fddb4228eb3311aded
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-17 23:22:58 +0530

    Leetcode Question of the Day Added

---
+++ Leetcode Daily Question of the Day/17) 1727. Largest Submatrix With Rearrangements.py
@@ -0,0 +1,11 @@
+'''
+Question: 1727
+Title: Largest Submatrix With Rearrangements
+Difficulty: Medium
+Given a binary matrix, we can rearrange the columns of the matrix in any order. After rearranging the columns, return the area of the largest submatrix that only contains 1's.
+Return the area of the largest submatrix within the rearranged matrix that only contains 1's.
+Example 1:
+Input: matrix = [[0,0,1],[1,1,1],[1,0,1]]
+Output: 4
+
+'''

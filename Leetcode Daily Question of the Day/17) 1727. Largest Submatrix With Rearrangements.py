925a7aba8c5d49a8e76dbe8f3f66fd1045db11d7
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-17 23:23:42 +0530

    Leetcode Question of the Day Added

--- Leetcode Daily Question of the Day/17) 1727. Largest Submatrix With Rearrangements.py
+++ Leetcode Daily Question of the Day/17) 1727. Largest Submatrix With Rearrangements.py
@@ -9,3 +9,22 @@ Input: matrix = [[0,0,1],[1,1,1],[1,0,1]]
 Output: 4
 
 '''
+
+class Solution:
+    def largestSubmatrix(self, matrix):
+        m, n = len(matrix), len(matrix[0])
+        
+        for i in range(1, m):
+            for j in range(n):
+                if matrix[i][j] == 1:
+                    matrix[i][j] += matrix[i-1][j]
+        
+        max_area = 0
+        
+        for i in range(m):
+            row = sorted(matrix[i], reverse=True)
+            
+            for j in range(n):
+                max_area = max(max_area, row[j] * (j + 1))
+        
+        return max_area

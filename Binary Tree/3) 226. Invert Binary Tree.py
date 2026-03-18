203e9c491d3d193bff882ef7ad842fbb39f9addb
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-18 23:45:04 +0530

    Inverse Binary Tree : Question Added

--- Binary Tree/3) 226. Invert Binary Tree.py
+++ Binary Tree/3) 226. Invert Binary Tree.py
@@ -1,4 +1,15 @@
+'''
+Question No. : 226
+Title : Invert Binary Tree
+Difficulty : Easy
 
+Given the root of a binary tree, invert the tree, and return its root.
+Example 1:
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+
+'''
 
 def invertTree(self, root):
     if not root:

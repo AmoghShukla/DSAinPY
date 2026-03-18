34fe3dbad318007dfe2e37ddb0a27000603875e3
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-18 23:48:52 +0530

    Minimum Depth on a Binary Tree : Code Added

--- Binary Tree/2) 111. Minimum Depth of Binary Tree.py
+++ Binary Tree/2) 111. Minimum Depth of Binary Tree.py
@@ -10,3 +10,16 @@ Input: root = [3,9,20,null,null,15,7]
 Output: 2
 
 '''
+
+def minDepth(self, root) -> int:
+        if not root:
+            return 0
+        if not root.left:
+            return self.minDepth(root.right) + 1
+        if not root.right:
+            return self.minDepth(root.left) + 1
+
+        else:
+            left = self.minDepth(root.left)
+            right = self.minDepth(root.right)
+        return min(left, right) + 1

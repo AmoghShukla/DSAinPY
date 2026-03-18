9913ab3f779cc6a33c0058f2e3a79d6dcdd36cc6
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-18 23:16:50 +0530

    Leetcode Question fo the Day : Code Added

--- Leetcode Daily Question of the Day/18) 3070. Count Submatrices with Top-Left Element and Sum Less Than k.py
+++ Leetcode Daily Question of the Day/18) 3070. Count Submatrices with Top-Left Element and Sum Less Than k.py
@@ -14,3 +14,20 @@ Explanation: The submatrices that contain the top-left element of the grid and h
 - The submatrix with the top-left element itself, which has a sum of 1.
 
 '''
+
+def countSubmatrices(self, grid: list[list[int]], k: int) -> int:
+    m, n = len(grid), len(grid[0])
+    counter = 0
+
+    for i in range(m):
+        for j in range(n):
+            top = grid[i-1][j] if i>0 else 0
+            left = grid[i][j=1] if j>0 else 0
+            diagonal = grid[i-1][j-1] if i>0 and j > 0 else 0
+
+            grid[i][j] += top + left - diagonal
+
+            if grid[i][j] <= k:
+                counter += 1
+
+    return counter

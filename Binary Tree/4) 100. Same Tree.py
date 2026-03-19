8d4d52ea3e00caf24519976525040973ae4296c2
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-19 22:33:18 +0530

    Binary Tree : Same Tree : Code Addeed

--- Binary Tree/4) 100. Same Tree.py
+++ Binary Tree/4) 100. Same Tree.py
@@ -11,3 +11,19 @@ Input: p = [1,2,3], q = [1,2,3]
 Output: true
 
 '''
+
+def isSameTree(self, p, q) -> bool:
+
+    if not p and not q:
+        return True
+    
+    if not p or not q:
+        return False
+    
+    if p.val != q.val:
+        return False
+    
+    return (
+        self.isSameTree(p.left, q.left) and 
+        self.isSameTree(p.right, q.right)
+    )

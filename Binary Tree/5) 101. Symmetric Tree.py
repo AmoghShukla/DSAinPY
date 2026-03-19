51935f8fbfbc3b0826c525f72c12d892af997897
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-19 22:48:42 +0530

    Binary Tree : Symmetric Tree : Code Addeed

--- Binary Tree/5) 101. Symmetric Tree.py
+++ Binary Tree/5) 101. Symmetric Tree.py
@@ -7,3 +7,24 @@ Input: root = [1,2,2,3,4,4,3]
 Output: true
 
 '''
+
+def isSymmetric(self, root) -> bool:
+
+    if not root:
+        return True
+    
+    def isMirror(t1, t2):
+
+        if not t1 and not t2:
+            return True
+        
+        if not t1 or not t2:
+            return False
+        
+        return (
+            t1.val == t2.val and
+            isMirror(t1.left, t2.right) and 
+            isMirror(t1.right, t2.left)
+        )
+    
+    return isMirror(root.left, root.right)

08bfb627f9dc17b64b391cc85027aa13143ea297
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-21 22:17:21 +0530

    Binary Tree : Level Order Traversal : Code Added

--- Binary Tree/6) 102. Binary Tree Level Order Traversal.py
+++ Binary Tree/6) 102. Binary Tree Level Order Traversal.py
@@ -9,3 +9,25 @@ Input: root = [3,9,20,null,null,15,7]
 Output: [[3],[9,20],[15,7]]
 
 '''
+from collections import deque
+def levelOrder(self, root):
+
+    if not root:
+        return []
+    
+    queue = deque([root])
+    result = []
+
+    while queue:
+        level = []
+
+        for _ in range(len(queue)):
+            node = queue.popleft()
+
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        result.append(level)
+    return result
